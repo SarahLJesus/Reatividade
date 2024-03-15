7c673dabceae3418460459d8bb45f2814499ff2f
SarahLJesus <sarahletjesus1908@gmail.com>
2024-03-15 10:37:41 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const contador = ref(0);
+
+  function incrementar() {
+    contador.value++;
+  }
+  function decrementar() {
+    contador.value--;
+  }
+
+</script>
+
+<template>
+  <button @click="incrementar">{{ contador }} </button>
+  <button @click="decrementar">{{ contador }} </button>
+</template>
